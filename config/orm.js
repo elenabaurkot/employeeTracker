541c26e07679b94f04532d258b0eafcbe3d76d5f
elenabaurkot <elenabaurkot@gmail.com>
2020-02-28 23:42:03 -0500

    add employee working, aside from manager id

--- config/orm.js
+++ config/orm.js
@@ -11,47 +11,14 @@ var orm = {
         var queryString = "INSERT INTO ?? (??) VALUES (?)";
         connection.query(queryString, [table, col, answer.charAt(0).toUpperCase() + answer.slice(1)], function(err, res) {
             if (err) throw err; 
-            console.log(res);
-            console.log(queryString);
         })
-    }
+    },
+    addNumber: function(table, col, answer){
+        var queryString = "INSERT INTO ?? (??) VALUES (?)";
+        connection.query(queryString, [table, col, answer], function(err, res) {
+            if (err) throw err; 
+        })
+    } 
 };
-
-
-// for the add role 
-
-
-
-//     selectWhere: function(tableInput, colToSearch, valOfCol) {
-//       var queryString = "SELECT * FROM ?? WHERE ?? = ?";
-//       connection.query(queryString, [tableInput, colToSearch, valOfCol], function(err, result) {
-//         if (err) throw err;
-//         console.table(result);
-//       });
-//     },
-//     selectAndOrder: function(whatToSelect, table, orderCol) {
-//       var queryString = "SELECT ?? FROM ?? ORDER BY ?? DESC";
-//       console.log(queryString);
-//       connection.query(queryString, [whatToSelect, table, orderCol], function(err, result) {
-//         if (err) throw err;
-//         console.table(result);
-//       });
-//     },
-//     findWhoHasMost: function(tableOneCol, tableTwoForeignKey, tableOne, tableTwo) {
-//       var queryString =
-//         "SELECT ??, COUNT(??) AS count FROM ?? LEFT JOIN ?? ON ??.??= ??.id GROUP BY ?? ORDER BY count DESC LIMIT 1";
-  
-//       connection.query(
-//         queryString,
-//         [tableOneCol, tableOneCol, tableOne, tableTwo, tableTwo, tableTwoForeignKey, tableOne, tableOneCol],
-//         function(err, result) {
-//           if (err) throw err;
-//           console.table(result);
-//         }
-//       );
-//     }
-//   };
-
-
 
 module.exports = orm;
--- server.js
+++ server.js
@@ -4,45 +4,45 @@ var connection = require("./config/connection");
 
 employeeTracker();
 
-  function employeeTracker() {
-    inquirer
-      .prompt({
-        name: "action",
-        type: "list",
-        message: "What would you like to do?",
-        choices: [
-          "View All Employees",
-          "View All Departments",
-          "View All Employee Roles",
-          "Add Department",
-          "Add Employee",
-          "Add Role", 
-          "Update Employee Role",
-          "Exit"
-        ]
-      })
-      .then(function(answer) {
-        switch (answer.action) {
+function employeeTracker() {
+  inquirer
+    .prompt({
+      name: "action",
+      type: "list",
+      message: "What would you like to do?",
+      choices: [
+        "View All Employees",
+        "View All Departments",
+        "View All Employee Roles",
+        "Add Department",
+        "Add Employee",
+        "Add Role",
+        "Update Employee Role",
+        "Exit"
+      ]
+    })
+    .then(function(answer) {
+      switch (answer.action) {
         case "View All Employees":
           viewEmployees();
           break;
-  
+
         case "View All Departments":
           viewDepartments();
           break;
-  
+
         case "View All Employee Roles":
           viewRoles();
           break;
-  
+
         case "Add Department":
           addDepartment();
           break;
-  
+
         case "Add Employee":
           addEmployee();
           break;
-        
+
         case "Add Role":
           addRole();
           break;
@@ -50,101 +50,129 @@ employeeTracker();
         case "Update Employee Role":
           updateEmployee();
           break;
-        
+
         case "Exit":
           connection.end();
           break;
-        }
-      });
-  }
+      }
+    });
+}
 // view all employees
-function viewEmployees(){
-    orm.selectAll('employee');
-    employeeTracker();
+function viewEmployees() {
+  orm.selectAll("employee");
+  employeeTracker();
 }
 // view all departments
-function viewDepartments(){
-  orm.selectAll('department');
+function viewDepartments() {
+  orm.selectAll("department");
   employeeTracker();
 }
-// view all employee roles 
-function viewRoles(){
-  orm.selectAll('role');
+// view all employee roles
+function viewRoles() {
+  orm.selectAll("role");
   employeeTracker();
 }
 
-function addDepartment(){
+// Add Department
+function addDepartment() {
   inquirer
-  .prompt({
-    name: "department",
-    type: "input",
-    message: "What department would you like to add?"
-   })
-   .then(function(answer) {
-      orm.add('department', 'name', answer.department);
+    .prompt({
+      name: "department",
+      type: "input",
+      message: "What department would you like to add?"
+    })
+    .then(function(answer) {
+      orm.add("department", "name", answer.department);
       employeeTracker();
-   });
-  }
+    });
+}
 
-
-function addEmployee(){
+// Add new employee
+function addEmployee() {
   // query the database for all items in the role table and make array of all roles
   connection.query("SELECT * FROM role", function(err, results) {
     if (err) throw err;
     var roleArray = [];
     if (err) throw err;
-      for (var i = 0; i < results.length; i++) {
-        roleArray.push(results[i].title);
+    for (var i = 0; i < results.length; i++) {
+      roleArray.push(results[i].title);
+    }
+    // query the database for all items in the employee table and make array of all managers
+    var managerArray = [];
+    connection.query(
+      "SELECT first_name, last_name, title FROM employee LEFT JOIN role ON employee.role_id = role.id WHERE title LIKE '%Manager%'",
+      function(err, resultsManager) {
+        if (err) throw err;
+        for (var i = 0; i < resultsManager.length; i++) {
+          managerArray.push(
+            resultsManager[i].first_name + " " + resultsManager[i].last_name
+          );
+        }
+        return managerArray;
       }
+    );
+    inquirer
+      .prompt([
+        {
+          name: "first",
+          type: "input",
+          message: "What is the employee's first name?"
+        },
+        {
+          name: "last",
+          type: "input",
+          message: "What is the employee's last name?"
+        },
+        {
+          name: "role",
+          type: "list",
+          message: "What is the employee's role?",
+          choices: roleArray
+        },
+        {
+          name: "manager",
+          type: "list",
+          message: "Who is the employee's manager?",
+          choices: managerArray
+        }
+      ])
+      .then(function(answer) {
+        var first = answer.manager.split(" ")[0];
+        var last = answer.manager.split(" ")[1];
 
-  // query the database for all items in the employee table and make array of all managers
-  // for loop to show the managers
-  // something along the lines of if role is like "%manager%" show name 
-  var managerArray = [];
-  connection.query("SELECT * FROM employee", function(err, resultsManager) {
-      for (var i = 0; i < resultsManager.length; i++) {
-        managerArray.push(resultsManager[i].first_name + ' ' + resultsManager[i].last_name);
-      }
-      return managerArray;
+        connection.query(
+          "SELECT id FROM employee WHERE first_name= ?, AND last_name= ?",
+          (last, first, last),
+          function(err, resultManagerID) {
+            if (err) throw err;
+            var managerID = resultManagerID[0].id;
+            console.log(managerID);
+
+            connection.query(
+              "SELECT id FROM role WHERE title= ?",
+              answer.role,
+              function(err, resultRoleId) {
+                if (err) throw err;
+                var roleID = resultRoleId[0].id;
+                console.log(roleID);
+
+                connection.query(
+                  "INSERT INTO employee (first_name, last_name, role_id) VALUES (?, ?, ?)",
+                  (answer.first, answer.last, roleID),
+                  function(err, data) {
+                    if (err) throw err;
+
+                    console.log(answer.first + " " + answer.last);
+                    console.log(answer.role);
+                    console.log(answer.manager);
+
+                    employeeTracker();
+                  }
+                );
+              }
+            );
+          }
+        );
+      });
   });
-  
-  inquirer
-  .prompt([
-    {
-    name: "first",
-    type: "input",
-    message: "What is the employee's first name?"
-   },
-   {
-    name: "last",
-    type: "input",
-    message: "What is the employee's last name?"
-   },
-   {
-    name: "role",
-    type: "list",
-    message: "What is the employee's role?",
-    choices: roleArray 
-  },
-   {
-    name: "manager",
-    type: "list",
-    message: "Who is the employee's manager?",
-    choices: managerArray 
-   }
-  ])
-   .then(function(answer) {
-  //   // orm.add('employee', 'first_name', answer.first);
-  //   // // orm.add('employee', 'last_name', answer.last);
-  //   // // shouldn't be answer.role, should be the id that corresponds with the role
-  //   // orm.add('employee', 'role_id', answer.role);
-  //   // // shouldn't be answer.role, should be the id that corresponds with the role
-  //   // orm.add('employee', 'manager_id', answer.manager);
-    console.log(answer.first + ' ' + answer.last);
-    console.log(answer.role);
-    console.log(answer.manager);
-
-    employeeTracker();
-   });
-});
 }
