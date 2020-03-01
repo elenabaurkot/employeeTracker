b79c91f6de678343ef521bcdf26ac9baf220edd8
elenabaurkot <elenabaurkot@gmail.com>
2020-03-01 02:04:08 -0500

    view employees by role working for all role types

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,11 @@
 var mysql = require("mysql");
+require('dotenv').config();
 
 var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
-    password: "sejc2828",
+    password: process.env.MYSQL_PW,
     database: "employeeTrackerDB"
   });
 
--- db/seed.sql
+++ db/seed.sql
@@ -11,10 +11,10 @@ INSERT INTO role (title, salary, department_id) VALUES('Accounting Manager', 900
 INSERT INTO role (title, salary, department_id) VALUES('Development Manager', 90000, 4);
 INSERT INTO role (title, salary, department_id) VALUES('Marketing Manager', 90000, 3);
 
-INSERT INTO employee(first_name, last_name, role_id, manager_id) VALUES('Eli', 'Smol', 2);
+INSERT INTO employee(first_name, last_name, role_id) VALUES('Eli', 'Smol', 2);
 INSERT INTO employee(first_name, last_name, role_id, manager_id) VALUES('Matt', 'Green', 3, 8);
 INSERT INTO employee(first_name, last_name, role_id, manager_id) VALUES('Sam', 'Smith', 4, 7);
-INSERT INTO employee(first_name, last_name, role_id, manager_id) VALUES('Elle', 'Woods', 2);
+INSERT INTO employee(first_name, last_name, role_id) VALUES('Elle', 'Woods', 2);
 INSERT INTO employee(first_name, last_name, role_id, manager_id) VALUES('Michael', 'Scott', 1, 6);
 INSERT INTO employee(first_name, last_name, role_id) VALUES('Sara', 'Patel', 7);
 INSERT INTO employee(first_name, last_name, role_id) VALUES('Mitch', 'Conors', 6);
